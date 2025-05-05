cc71be2a8d8594d5164545adc7d9ef045b79bfc6
Akhtar Abbas <akhtarabbas94874@gmail.com>
2025-05-05 18:55:49 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,5 +39,5 @@ if ingredient_list:
         #session.sql(my_insert_stmt).collect()
         #st.success('Your Smoothie is ordered!', icon="✅")
 
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"+fruit_chosen)
     st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
