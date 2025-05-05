7a1d1a8afdd1c1e682a43eef1d3f739e0a035d7d
Akhtar Abbas <akhtarabbas94874@gmail.com>
2025-05-05 18:28:06 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,11 +45,11 @@ if ingredient_list:
 
     #st.write(my_insert_stmt)
     #st.stop()
-       time_to_insert=st.button('Submit Order')
-      if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        st.success('Your Smoothie is ordered!', icon="✅")
-      import requests
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        time_to_insert=st.button('Submit Order')
+        if time_to_insert:
+          session.sql(my_insert_stmt).collect()
+          st.success('Your Smoothie is ordered!', icon="✅")
+        import requests
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-      sf_df=st.dataframe(data=smoothiefroot_response.json()  ,use_container_width=True)
+         sf_df=st.dataframe(data=smoothiefroot_response.json()  ,use_container_width=True)
